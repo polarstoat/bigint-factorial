43ff3d09475bd18dffe36298e7e25eaae5075643
Polar stoat <polarstoat@users.noreply.github.com>
2020-09-10 17:26:19 +0100

    Add JSDoc comments

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/**
+ * Calculate the factorial of `n`
+ *
+ * @param  {BigInt} n The number to calculate the factorial of
+ * @return {BigInt}   The factorial of the passed number
+ */
 function factorial(n) {
   if (n === 0n) return 1n;
 
