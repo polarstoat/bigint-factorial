18aeac79dc12a945e75f3acbdf8e33addd68f8bd
Polar stoat <polarstoat@users.noreply.github.com>
2020-09-26 16:52:11 +0100

    perf: Add factorial functions using for/while loops

---
+++ factorialWhile.js
@@ -0,0 +1,17 @@
+function factorialWhile(n) {
+  if (typeof n !== 'bigint') throw new TypeError(`Expected parameter type of 'bigint' for n; instead got '${typeof n}'`);
+
+  if (n === 0n) return 1n;
+
+  let result = n;
+  let i = result;
+
+  while (i > 1n) {
+    i -= 1n;
+    result *= i;
+  }
+
+  return result;
+}
+
+module.exports = factorialWhile;
